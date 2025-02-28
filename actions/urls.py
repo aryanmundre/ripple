8589eca87fbff3929f1a99d849496dfc2bc317df
Jeffrey Liu <jeffreyliu@Jeffreys-MacBook-Pro.local>
2025-02-28 04:20:40 -0800

    fixed actions redundancy in api name

--- actions/urls.py
+++ actions/urls.py
@@ -10,9 +10,9 @@ from .views import (
 )
 
 urlpatterns = [
-    path("actions/feed/", ActionFeedView.as_view(), name="action-feed"),  # New API for fetching all actions
-    path("actions/category/", ActionByCategoryView.as_view(), name="actions-by-category"),  # Filter actions by category
-    path("actions/trending/", RecommendationActionListView.as_view(), name="trending-actions"),  # Get trending actions
+    path("feed/", ActionFeedView.as_view(), name="action-feed"),  # New API for fetching all actions
+    path("category/", ActionByCategoryView.as_view(), name="actions-by-category"),  # Filter actions by category
+    path("trending/", RecommendationActionListView.as_view(), name="trending-actions"),  # Get trending actions
     path("user-actions/", UserActionView.as_view(), name="user-actions"),  # Create & retrieve user interactions
     path("user-actions/list/", UserActionListView.as_view(), name="user-actions-list"),  # List actions a user has completed
     path("user-actions/remove/", RemoveUserActionView.as_view(), name="remove-user-action"),  # Remove a user action
--- gamification/urls.py
+++ gamification/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import GamificationDataView, AwardPointsView
 
 urlpatterns = [
-    path('gamification/', GamificationDataView.as_view(), name='gamification-data'),
+    path('gamification_data/', GamificationDataView.as_view(), name='gamification-data'),
     path('award-points/', AwardPointsView.as_view(), name='award-points'),
 ]
 
