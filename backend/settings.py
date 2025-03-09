a18ad6eab9b27f42912ffa81efe984297eafad03
Jeffrey Liu <jeffreyliu@Jeffreys-MacBook-Pro.local>
2025-03-09 14:58:21 -0700

    fixed firebase and added render db

--- backend/settings.py
+++ backend/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-import custom_auth.firebase_config  # This runs the initialization code
+#import custom_auth.firebase_config  # This runs the initialization code
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = "/static/"
