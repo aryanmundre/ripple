1c4a4ec9b4eb8d59d3bdf33310ae729fa60ebacb
Jeffrey Liu <jeffreyliu@Jeffreys-MacBook-Pro.local>
2025-03-09 14:48:31 -0700

    testing

--- Ripple/components/index.js
+++
@@ -1,6 +0,0 @@
-import TestComponent from './TestComponent.jsx';
-import Login from './Login.jsx';
-import ScreenHeaderBtn from './ScreenHeaderBtn.jsx';
-import OrgCard from './explore/OrgCard.jsx'
-
-export { TestComponent, Login, ScreenHeaderBtn, OrgCard };
